25b165b65f8bf9ad3bf8376f968fe7208dfd4df0
Ali Hayder <ali.hayder.rzayyig@gmail.com>
2022-03-25 17:08:04 +0300

    finsh CH7

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,20 +1,31 @@
 <template>
-  <h1>Job Details Page</h1>
-  <!-- <p>the job id is {{ $route.params.id }}</p> -->
-  <p>the job id is {{ id }}</p>
-
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <!-- <p>the job id is {{ $route.params.id }}</p> -->
+    <p>the job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <dir v-else>
+    <p>loading job.....</p>
+  </dir>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            id: this.$route.params.id
-        }
-    },
-}
+  data() {
+    return {
+      id: this.$route.params.id,
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err));
+  },
+};
 </script>
 
 <style>
-
 </style>
